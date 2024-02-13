c26c51b0cf5cdef74505f24b248b5a121d8dd0ee
Eloi Aymerich Berenguer <eaymerich@hiberus.com>
2024-02-13 12:16:28 +0100

    Modificación pagina principal resumen sincronización

--- docs/script.js
+++ docs/script.js
@@ -6,11 +6,13 @@ document.addEventListener('DOMContentLoaded', function() {
           data.forEach(execution => {
               const row = document.createElement('tr');
               row.innerHTML = `
-                  <td>${execution.fechaHora}</td>
-                  <td>${execution.tareasCreadas}</td>
-                  <td>${execution.tareasModificadas}</td>
-                  <td>${execution.estado}</td>
-                  <td><a href="${execution.informe}" target="_blank">Informe</a></td>
+                <td>${execution.fechaHora}</td>
+                <td>${execution.tareasCreadas}</td>
+                <td>${execution.tareasModificadas}</td>
+                <td>${execution.tareasFallidas}</td>
+                <td>${execution.tareasNoModificadas}</td>
+                <td>${execution.estado}</td>
+                <td><a href="${execution.informe}" target="_blank">Informe</a></td>
               `;
               tbody.appendChild(row);
           });
